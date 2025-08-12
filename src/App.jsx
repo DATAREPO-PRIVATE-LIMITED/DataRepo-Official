e28bb7545b5f30aa54e15c2eda0191ab992d6749
yadavabhi12911 <yadavabhi12911@gmail.com>
2025-08-12 18:42:25 +0530

    DataRepo Official basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-teal-600 text-7xl m-auto text-center font-semibold'>DataRepo Officails </h1>
+    </div>
+  )
+}
+
+export default App
