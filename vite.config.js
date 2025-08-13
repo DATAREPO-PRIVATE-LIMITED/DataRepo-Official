bdd37d133df6ac3c13363b268fdd61a824b0561d
yadavabhi12911 <yadavabhi12911@gmail.com>
2025-08-13 20:48:32 +0530

    update config file

--- vite.config.js
+++ vite.config.js
@@ -12,23 +12,3 @@ export default defineConfig({
     },
   },
 })
-
-
-
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-      },
-      animation: {
-        marquee: "marquee 15s linear infinite",
-      },
-    },
-  },
-  plugins: [],
-};
