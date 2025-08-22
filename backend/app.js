801a88a935a087dd234d2d632ac544055c07ca7e
yadavabhi12911 <yadavabhi12911@gmail.com>
2025-08-22 19:12:55 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://apimarketplace-pi.vercel.app',
+    origin: 'https://apimarketplace-pi.vercel.app/',
     // origin: 'http://localhost:5173',
     credentials: true
 }))
