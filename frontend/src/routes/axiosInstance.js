854592dfb4f5d44897a0dbcf94f00f640a9f7ebd
yadavabhi12911 <yadavabhi12911@gmail.com>
2025-08-22 19:10:25 +0530

    update backend url

--- frontend/src/routes/axiosInstance.js
+++ frontend/src/routes/axiosInstance.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 
 export const AxiosInstance = axios.create({
-    baseURL: 'https://apimarketplace-pi.vercel.app/api'
+    baseURL: 'https://datarepo-official.onrender.com/api'
     // baseURL: 'http://localhost:9000/api'
 })
