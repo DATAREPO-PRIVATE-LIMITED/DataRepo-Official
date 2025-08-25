cc56b5adccec8a4355f31e9caedb74ee0b4d5ee3
yadavabhi12911 <yadavabhi12911@gmail.com>
2025-08-25 20:53:31 +0530

    fix bugs

--- frontend/src/routes/axiosInstance.js
+++ frontend/src/routes/axiosInstance.js
@@ -2,10 +2,11 @@
 import axios from 'axios'
 
 export const AxiosInstance = axios.create({
-    baseURL: 'https://datarepo-official.onrender.com/api',
-    // baseURL: 'http://localhost:9000/api',
+    // baseURL: 'https://datarepo-official.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
 })
+
